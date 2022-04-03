2b8ed24d76a4a53aa9768a876154447ed54b00a3
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-04-03 21:41:46 +0200

    Declare string variables

---
+++ ex12/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    //Only change code below this line
+    var myFirstName = "Sanjin";
+    var myLastName = "Suljanovic";
+    var myHomeTown = "Bosanska Krupa";
+   //Only change code above this line
+    
+   return {
+       myFirstName,
+       myLastName,
+       myHomeTown
+   };
+}
+
+console.log(main());
+module.exports = main;
