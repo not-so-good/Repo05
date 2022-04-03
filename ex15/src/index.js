15ff57a62aa5a5ad76401c80ab243640ccb5b408
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-04-03 23:29:09 +0200

    Word blanks

---
+++ ex15/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    //Only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = "My " + myAdjective + " " +  myNoun + " " + myVerb + " " + myAdverb + " in front of me, while I tried to catch up" + ".";
+   //Only change code above this line
+    
+   return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
